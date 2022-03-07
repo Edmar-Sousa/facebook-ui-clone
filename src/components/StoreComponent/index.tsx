10bc29299c09e9432cce33c63f965315c9d0998a
Edmar-Sousa <edmarsousamendes4@gmail.com>
2022-03-07 16:07:19 -0300

    files

--- src/components/StoreComponent/index.tsx
+++ src/components/StoreComponent/index.tsx
@@ -1,21 +1,69 @@
 
+import { BsPlusCircleFill } from 'react-icons/bs'
+
 import {
-    StoreContainer,
-    StoreCard
+    StoryContainer,
+    StoryCard,
+    FooterCard,
+    AddStoryPlusBtn,
+    TitleStory,
+    UserStory,
+    UserImageContainer
 } from './style'
 
 const StoreAreaComponent = () => {
     return (
-        <StoreContainer>
-            <StoreCard>
-                
-            </StoreCard>
+        <StoryContainer>
+            <StoryCard>
+                <FooterCard>
+                    <AddStoryPlusBtn>
+                        <BsPlusCircleFill fontSize={34} color="#1c75eb" />
+                    </AddStoryPlusBtn>
 
-            <StoreCard />
-            <StoreCard />
-            <StoreCard />
-            <StoreCard />
-        </StoreContainer>
+                    <TitleStory textAlign="center">Criar story</TitleStory>
+                </FooterCard>
+            </StoryCard>
+
+            <StoryCard>
+                <UserStory>
+                    <UserImageContainer />
+                </UserStory>
+
+                <FooterCard FillBg='#0000'>
+                    <TitleStory textAlign="left" color="#fff">Criar story</TitleStory>
+                </FooterCard>
+            </StoryCard>
+
+            <StoryCard>
+                <UserStory>
+                    <UserImageContainer />
+                </UserStory>
+
+                <FooterCard FillBg='#0000'>
+                    <TitleStory textAlign="left" color="#fff">Criar story</TitleStory>
+                </FooterCard>
+            </StoryCard>
+
+            <StoryCard>
+                <UserStory>
+                    <UserImageContainer />
+                </UserStory>
+
+                <FooterCard FillBg='#0000'>
+                    <TitleStory textAlign="left" color="#fff">Criar story</TitleStory>
+                </FooterCard>
+            </StoryCard>
+
+            <StoryCard>
+                <UserStory>
+                    <UserImageContainer />
+                </UserStory>
+
+                <FooterCard FillBg='#0000'>
+                    <TitleStory textAlign="left" color="#fff">Criar story</TitleStory>
+                </FooterCard>
+            </StoryCard>
+        </StoryContainer>
     )
 }
 
--- src/components/StoreComponent/style.ts
+++ src/components/StoreComponent/style.ts
@@ -1,24 +1,101 @@
 import styled from 'styled-components'
 
 
-export const StoreContainer = styled.div`
+export const StoryContainer = styled.div`
     width: 80%;
     margin: auto;
     height: 200px;
     background: #ff0;
 
     display: flex;
-    justify-content: space-between;
 `
 
-export const StoreCard = styled.div`
+export const StoryCard = styled.div`
     width: 120px;
     height: 100%;
 
     border-radius: 10px;
     background: #000;
+
+    margin: 0 5px;
+
+    position: relative;
 `
 
+interface FooterCardPropType {
+    FillBg ?: string
+}
+
 export const FooterCard = styled.div`
-    
+    position: absolute;
+    width: 100%;
+    height: 50px;
+    bottom: 0;
+
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+
+    background: ${(props : FooterCardPropType) => props.FillBg ? props.FillBg : '#fff' };
+
+    &:first-child p {
+        margin-left: 0;
+    }
+`
+
+export const AddStoryPlusBtn = styled.div`
+    position: absolute;
+    width: 40px;
+    height: 40px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    top: -20px;
+    left: 40px;
+    border-radius: 50%;
+
+    background: #fff;
+`
+
+interface TitleStoryPropsType {
+    textAlign : 'center' | 'left'
+    color ?: string
+}
+
+export const TitleStory = styled.p`
+    font-family: Arial;
+    font-size: 11pt;
+
+    font-weight: bold;
+    color: ${(props : TitleStoryPropsType) => props.color ? props.color : '#000'};
+
+    text-align: ${(props : TitleStoryPropsType) => props.textAlign};
+    margin-top: 20px;
+
+    margin-left: 10px;
+`
+
+export const UserStory = styled.div`
+    width: 40px;
+    height: 40px;
+
+    border-radius: 20px;
+    position: absolute;
+    top: 10px;
+    left: 10px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background: #1c75eb;
+`
+
+export const UserImageContainer = styled.div`
+    width: 32px;
+    height: 32px;
+
+    border-radius: 20px;
+    background: #fff;
 `
